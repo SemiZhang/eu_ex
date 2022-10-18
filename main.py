7ee5ebd50b186ea8cb60e4c606d671456130a7a0
SemiZhang <zsy455461505@gmail.com>
2022-10-18 17:39:05 +0200

    Use Secret for Appraise

--- main.py
+++ main.py
@@ -112,9 +112,9 @@ LOGIN_MAX_RETRY_COUNT = 5
 
 # Telegram Bot Push https://core.telegram.org/bots/api#authorizing-your-bot
 # Obtained via @BotFather application, for example: 1077xxx4424:AAFjv0FcqxxxxxxgEMGfi22B4yh15R5uw
-TG_BOT_TOKEN = ""
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
 # User, group or channel ID, for example: 129xxx206
-TG_USER_ID = ""
+TG_USER_ID = os.environ.get("TG_USER_ID")
 # Build your own API reverse proxy address for use when the network environment is inaccessible,
 # and keep the default if the network is normal.
 TG_API_HOST = "https://api.telegram.org"
